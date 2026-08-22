02d6e9d60ea5bad5d0906cc85115b69cfde1389a
JAAT <canwingamers@gmail.com>
2026-08-22 13:42:10 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.spotify.mine',
+  appName: 'spotifymine',
+  webDir: 'dist'
+};
+
+export default config;
